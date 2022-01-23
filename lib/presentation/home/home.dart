06102f256b9d7dfbbadd7e9ba30a664f90d50690
ykkd <17854586+ykkd@users.noreply.github.com>
2022-01-23 23:03:36 +0900

    added Card1

---
+++ lib/presentation/home/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../parts/card/card1.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  List<Widget> pages = <Widget>[
+    const Card1(),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fooderlich', style: Theme.of(context).textTheme.headline6),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: <BottomNavigationBarItem>[
+            // ignore: lines_longer_than_80_chars
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card'),
+            // ignore: lines_longer_than_80_chars
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card2'),
+            // ignore: lines_longer_than_80_chars
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card3'),
+          ]),
+    );
+  }
+}
---
+++ lib/presentation/parts/card/card1.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Card1 extends StatelessWidget {
+  final String category = "Editor's Choice";
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread.';
+  final String chef = 'Ray Wenderlich';
+
+  const Card1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(width: 350, height: 450),
+        // ignore: prefer_const_constructors
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage('assets/mag1.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+        ),
+      ),
+    );
+  }
+}
