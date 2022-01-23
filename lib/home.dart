6bb56ccb57b23910c3acde82c69fbeee70786970
ykkd <17854586+ykkd@users.noreply.github.com>
2022-01-23 22:25:06 +0900

    added tabs

--- lib/home.dart
+++ lib/home.dart
@@ -8,16 +8,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  List<Widget> pages = <Widget>[
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Fooderlich', style: Theme.of(context).textTheme.headline6),
       ),
-      body: Center(
-        child: Text("Let's go cooking",
-            style: Theme.of(context).textTheme.headline1),
-      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: <BottomNavigationBarItem>[
+            // ignore: lines_longer_than_80_chars
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card'),
+            // ignore: lines_longer_than_80_chars
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card2'),
+            // ignore: lines_longer_than_80_chars
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card3'),
+          ]),
     );
   }
 }
